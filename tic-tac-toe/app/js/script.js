fb3843ecd7f429e680bdb3bf097e9a69668b1bb9
abilardo <don.abilardo@gmail.com>
2022-10-06 18:25:54 +0500

    Add function Win() and count for draw

--- tic-tac-toe/app/js/script.js
+++ tic-tac-toe/app/js/script.js
@@ -3,6 +3,7 @@ let game = document.querySelector('.game'),
     newGameButton = document.querySelector('.new-game'),
     fields = document.querySelectorAll('.field'),
     step = false,
+    count = 0,
     circle = `<svg class="circle"><circle r="45" cx="58" cy="58" stroke="blue" stroke-width="10" fill="none" stroke-linecap="round" /></svg>`,
     cross = `<svg class="cross"><line class="first" x1="15" y1="15" x2="100" y2="100" stroke="red" stroke-width="10" stroke-linecap="round" /><line class="second" x1="100" y1="15" x2="15" y2="100" stroke="red" stroke-width="10" stroke-linecap="round" /></svg>`;
 
@@ -11,12 +12,14 @@ function stepCross(target) {
     target.classList.add('x');
     let crossAudio = new Audio('tic-tac-toe/audio/cross.mp3');
     crossAudio.play();
+    count++;
 }
 function stepZero(target) {
     target.innerHTML = circle;
     target.classList.add('o');
     let circleAudio = new Audio('tic-tac-toe/audio/zero.mp3');
     circleAudio.play();
+    count++;
 }
 function init(event) {
     if (!step) stepCross(event.target)
@@ -27,6 +30,8 @@ function init(event) {
 function newGame() {
 
 }
+
+
 function win() {
     let comb = [
         [0, 1, 2],
@@ -38,7 +43,33 @@ function win() {
         [0, 4, 8],
         [2, 4, 6]
     ];
-
+    for (let i = 0; i < comb.length; i++) {
+        if (fields[comb[i][0]].classList.contains('x') &&
+            fields[comb[i][1]].classList.contains('x') &&
+            fields[comb[i][2]].classList.contains('x')) {
+            setTimeout(() => {
+                fields[comb[i][0]].classList.add('active_field');
+                fields[comb[i][1]].classList.add('active_field');
+                fields[comb[i][2]].classList.add('active_field');
+                res.innerText = 'Выйграли кресты';
+            }, 1500);
+            game.removeEventListener('click', init)
+        }
+        else if (fields[comb[i][0]].classList.contains('o') &&
+            fields[comb[i][1]].classList.contains('o') &&
+            fields[comb[i][2]].classList.contains('o')) {
+            setTimeout(() => {
+                fields[comb[i][0]].classList.add('active_field');
+                fields[comb[i][1]].classList.add('active_field');
+                fields[comb[i][2]].classList.add('active_field');
+                res.innerText = 'Выйграли ноли';
+            }, 1500);
+            game.removeEventListener('click', init)
+        }
+        else if (count == 9) {
+            res.innerText = 'Ничья';
+        }
+    }
 
 }
 
